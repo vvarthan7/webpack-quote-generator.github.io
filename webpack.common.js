42857a1014abeee9886b12484e09b5e73cc701ca
VISHNUVARTHAN U <vvarthan7@gmail.com>
2022-07-23 11:20:02 +0530

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -6,17 +6,6 @@ module.exports = {
   entry: {
     bundle: path.resolve(__dirname, 'src/index.js')
   },
-  devtool: 'source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist')
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback : true
-  },
   module: {
     rules: [
       {
